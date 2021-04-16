ac25bba4263cf3236a83e4ca255a3771b44c0c43
hani <haniyousfi@gmail.com>
2021-04-17 00:36:02 +0100

    Edit admin file

--- ocRnn/admin.py
+++ ocRnn/admin.py
@@ -1,7 +1,5 @@
 from django.contrib import admin
-from django.db.models import Q
-from django.utils.translation import gettext_lazy as _
-from ocRnn.core.recognition import CharRecognizer
+from ocRnn.core.ocr import CharRecognizer
 
 from .models import ImageModel
 
@@ -23,32 +21,32 @@ class InputFilter(admin.SimpleListFilter):
         yield all_choice
 
 
-class CategoryFilter(InputFilter):
-    parameter_name = 'category'
-    title = _('category')
-
-    def queryset(self, request, queryset):
-        if self.value() is not None:
-            category = self.value()
-            return queryset.filter(
-                Q(category=category)
-            )
+# class CategoryFilter(InputFilter):
+#     parameter_name = 'category'
+#     title = _('category')
+#
+#     def queryset(self, request, queryset):
+#         if self.value() is not None:
+#             category = self.value()
+#             return queryset.filter(
+#                 Q(category=category)
+#             )
 
 
 class ImageModelAdmin(admin.ModelAdmin):
     search_fields = [field.name for field in ImageModel._meta.get_fields()]
     list_display = [field.name for field in ImageModel._meta.get_fields()]
-    list_filter = ('processed', 'upload_date', CategoryFilter,)
-    actions = ('run_classification', )
+    list_filter = ('processed', 'upload_date',)
+    actions = ('run_ocr', )
 
-    def run_classifier(self, image):
-        classifier = CharRecognizer(image)
-        classifier.get_dog_category()
+    def run_extractor(self, image):
+        ocr = CharRecognizer(image)
+        ocr.run()
 
-    def run_classification(self, *args, **kwargs):
+    def run_ocr(self, *args, **kwargs):
         non_processed = ImageModel.objects.filter(processed=False)[:2]
         for image in non_processed:
-            self.run_classifier(image)
+            self.run_extractor(image)
 
 
 admin.site.register(ImageModel, ImageModelAdmin)
